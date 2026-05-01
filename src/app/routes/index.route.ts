156e90d94cd437566c04fff9d7a71b512cbe14ff
Abidur Rahman Arko <aroarko.sd@gmail.com>
2026-05-01 23:33:36 +0800

    bike routes linked to centralized route

--- src/app/routes/index.route.ts
+++ src/app/routes/index.route.ts
@@ -3,11 +3,16 @@ import express from "express";
 const router = express.Router();
 
 import { customerRoutes } from "../modules/Customer/customer.route";
+import { BikeRoutes } from "../modules/Bike/bike.route";
 
 const moduleRoutes = [
   {
     path: "/customers",
     route: customerRoutes,
+  },
+  {
+    path: "/bikes",
+    route: BikeRoutes,
   },
 ];
 
